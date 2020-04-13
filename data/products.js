4b0929d9e7a7ec0cc0422b9bf74168e9db10c1d0
Hunter Danielson <hunterleedanielson@gmail.com>
2020-04-13 16:14:00 -0700

    added data and images

---
+++ data/products.js
@@ -0,0 +1,102 @@
+export const productData = [
+    {
+        id: 'bloodbath-mat',
+        name: 'Bloodbath mat',
+        image: '../images/Bloodbath-Color-Changing-Bath-Mat4.gif',
+    },
+    {
+        id: 'can-of-whoop-ass',
+        name: 'Can of whoop ass',
+        image: '../images/can-of-whoop-ass1.gif',
+    },
+    {
+        id: 'canned-wifi',
+        name: 'Canned wifi',
+        image: '../images/canned-wifi-invisibles-pranks-gags-and-gifts-300x250.jpg',
+    },
+    {
+        id: 'everything-i-want-to-do',
+        name: 'Everything I want to do',
+        image: '../images/everything-i-want-to-do-is-illegal-book-300x250.jpg',
+    },
+    {
+        id: 'handy-hand',
+        name: 'Handy hand',
+        image: '../images/handy-hand.gif',
+    },
+    {
+        id: 'hay-in-needle',
+        name: 'Hay in a needle stack',
+        image: '../images/hay-in-a-needle-stack-game-prank-pack-300x250.jpg',
+    },
+    {
+        id: 'nanoleaf-square',
+        name: 'Nanoleaf square',
+        image: '../images/nanoleaf-square-300x250.jpg',
+    },
+    {
+        id: 'offensive-business',
+        name: 'Offensive business cards',
+        image: '../images/offensive-business-cards1-300x250.jpg',
+    },
+    {
+        id: 'passive-aggressive',
+        name: 'Passive agressive notes',
+        image: '../images/passive-aggressive-notes-knock-knock-300x250.jpg',
+    },
+    {
+        id: 'spherical-maker',
+        name: 'Spherical Maker',
+        image: '../images/spherical-alt-300x250.jpg',
+    },
+    {
+        id: 'stainless-steel-flicker',
+        name: 'Stainless steel flicker',
+        image: '../images/stainless-steel-wearable-flicking-device-300x250.jpg',
+    },
+    {
+        id: 'no-phone-air',
+        name: 'No phone air',
+        image: '../images/the-nophone-air-300x250.jpg',
+    },
+    {
+        id: 'roto-wipe',
+        name: 'Roto wipe',
+        image: '../images/the-roto-wipe-prank-pack-300x250.jpg',
+    },
+    {
+        id: 'unbreakable-pencil',
+        name: 'Unbreakable pencil',
+        image: '../images/the-unbreakable-mechanical-penicl-zebra-pen-300x250.jpg',
+    },
+    {
+        id: 'worst-alphabet-book',
+        name: 'Worst alphabet book',
+        image: '../images/the-worst-alphabet-book-ever-P-Is-for-Pterodactyl-300x250.jpg',
+    },
+    {
+        id: 'twelve-thousand-piece-puzzle',
+        name: '12,000 piece puzzle',
+        image: '../images/twelve-thousand-piece-puzzle-prank-pack-300x250.jpg',
+    },
+    {
+        id: 'underground-dog-house',
+        name: 'Underground dog house',
+        image: '../images/underground-dog-house-dogeden1-300x250.jpg',
+    },
+    {
+        id: 'usb-kill-stick',
+        name: 'Usb kill stick',
+        image: '../images/usb-kill-stick-destroys-any-pc-300x250.jpg',
+    },
+    {
+        id: 'air-guitar',
+        name: 'Air guitar',
+        image: '../images/wearable-air-guitar-300x250.jpg',
+    },
+    {
+        id: 'werewolk-dog-muzzle',
+        name: 'Werewolf dog muzzle',
+        image: '../images/werewolf-dog-muzzle-300x250.jpg',
+    }, 
+];
