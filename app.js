021a32b790fbcd9b9946c8c73aa61c47a250f787
Hunter Danielson <hunterleedanielson@gmail.com>
2020-04-13 16:30:06 -0700

    html and dom getting

---
+++ app.js
@@ -0,0 +1,6 @@
+import { productData } from './data/products.js';
+
+// grab from dom
+const imageTags = document.querySelectorAll('img');
+const radioTags = document.querySelectorAll('input');
+const products = new ProductsArray(productData);
