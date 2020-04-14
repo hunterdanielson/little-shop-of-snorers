4386e35b8fc7cd7b5dab7836777ad00644a7ec6b
Hunter Danielson <hunterleedanielson@gmail.com>
2020-04-14 16:49:26 -0700

    added graphs and weird non-duplication rules

--- app.js
+++ app.js
@@ -8,19 +8,27 @@ const radioTags = document.querySelectorAll('input');
 const nextButton = document.getElementById('next-button');
 const products = new ProductArray(productData);
 
+// weird no repeat picture rule
+let randomProductStore = products.getRandomProduct();
+let randomProductStore2 = products.getRandomProduct();
+let randomProductStore3 = products.getRandomProduct();
+
 const initializeNewButtons = () => {
     const randomProduct = products.getRandomProduct();
     let randomProduct2 = products.getRandomProduct();
     // get a new product for #2 thats different than #1
-    while (randomProduct2.id === randomProduct.id) {
+    while (randomProduct2.id === randomProduct.id || randomProduct2.id === randomProductStore.id || randomProduct2.id === randomProductStore2.id || randomProduct2.id === randomProductStore3.id) {
         randomProduct2 = products.getRandomProduct();
     }
     
     let randomProduct3 = products.getRandomProduct();
     // get a new product for #3 thats diff from #2 and #1
-    while (randomProduct3.id === randomProduct2.id || randomProduct3.id === randomProduct.id) {
+    while (randomProduct3.id === randomProduct.id || randomProduct3.id === randomProduct2.id || randomProduct3.id === randomProductStore.id || randomProduct3.id === randomProductStore2.id || randomProduct3.id === randomProductStore3.id) {
         randomProduct3 = products.getRandomProduct();
     }
+    randomProductStore = randomProduct;
+    randomProductStore2 = randomProduct2;
+    randomProductStore3 = randomProduct3;
     
     imageTags.forEach((imageTag, i) => {
         if (i === 0) {
@@ -75,7 +83,7 @@ function updateChoice() {
     // put the new displayed array into local storage
     addLocalDisplay(allDisplayed);
     // after this many inputs change to results page
-    if (storedPicks.length >= 25) {
+    if (storedPicks.length >= 3) {
         window.location.href = './results';
     }
 
--- results/results.js
+++ results/results.js
@@ -1,33 +1,264 @@
 import { getPicks, getDisplayed, getTotalPicks, getTotalDisplayed } from '../local-storage-api.js';
 
-const sessionPicked = document.getElementById('session-results-picked');
-const sessionDisplayed = document.getElementById('session-results-displayed');
-const totalPicked = document.getElementById('total-results-picked');
-const totalDisplayed = document.getElementById('total-results-displayed');
 const startOverButton = document.getElementById('start-over-button');
+const resetLocalStorage= document.getElementById('reset-local-storage');
+
 
 const localPicks = getPicks();
 const localDisplay = getDisplayed();
 const totalPicks = getTotalPicks();
 const totalDisplay = getTotalDisplayed();
 
-totalPicks.push(localPicks);
-totalDisplay.push(localDisplay);
+// push the data from this session onto totals array
+localPicks.forEach(pick => {
+    totalPicks.push(pick);
+});
 
-sessionPicked.textContent = localPicks;
-sessionDisplayed.textContent = localDisplay;
+localDisplay.forEach(display => {
+    totalDisplay.push(display);
+});
 
-totalDisplayed.textContent = totalDisplay;
-totalPicked.textContent = totalPicks;
-
+// set the totals into local storage
 let storeTotalPicks = JSON.stringify(totalPicks);
 localStorage.setItem('TOTALPICKS', storeTotalPicks);
 
 let storeTotalDisplay = JSON.stringify(totalDisplay);
 localStorage.setItem('TOTALDISPLAYED', storeTotalDisplay);
 
+function findById(items, id) {
+    // loop the array
+    for (let i = 0; i < items.length; i++) {
+        const item = items[i];
+
+        // check the id against item.id
+        if (item.id === id) {
+            return item;
+        }
+    }
+
+    // loop done, nothing found
+    return null;
+}
+
+// putting the data into a more easily useable format
+const sessionVotes = [];
+const totalVotes = [];
+
+function addInitialVoteItem(votes, id) {
+    const voteItem = {
+        id,
+        timesSeen: 0,
+        timesPicked: 0
+    };
+    votes.push(voteItem);
+}
+
+function incrementTimesSeen(votes, id) {
+    let temp = findById(votes, id);
+    if (!temp) {
+        addInitialVoteItem(votes, id);
+        temp = findById(votes, id);
+    }
+    temp.timesSeen++;
+}
+
+function incrementTimesPicked(votes, id) {
+    let temp = findById(votes, id);
+    if (!temp) {
+        addInitialVoteItem(votes, id);
+        temp = findById(votes, id);
+    }
+    temp.timesPicked++;
+}
+
+// loop through local picks in local storage and push each time item is picked
+localPicks.forEach(pick => {
+    incrementTimesPicked(sessionVotes, pick);
+});
+
+// loop through local picks in local storage and push each time item is displayed
+localDisplay.forEach(display => {
+    incrementTimesSeen(sessionVotes, display);
+});
+
+// same thing as session but for total
+totalPicks.forEach(pick => {
+    incrementTimesPicked(totalVotes, pick);
+});
+
+totalDisplay.forEach(display => {
+    incrementTimesSeen(totalVotes, display);
+});
+
+// session info for graphs
+const graphIds = sessionVotes.map(el => el.id);
+const graphSessionPicked = sessionVotes.map(el => el.timesPicked);
+const graphSessionSeen = sessionVotes.map(el => el.timesSeen);
+
+var ctx = document.getElementById('session-results-picked').getContext('2d');
+new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: graphIds,
+        datasets: [{
+            label: '# of Votes',
+            data: graphSessionPicked,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+var sesDisplay = document.getElementById('session-results-displayed').getContext('2d');
+new Chart(sesDisplay, {
+    type: 'bar',
+    data: {
+        labels: graphIds,
+        datasets: [{
+            label: '# of Times Seen',
+            data: graphSessionSeen,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+// all time info for graphs
+const totalGraphIds = totalVotes.map(el => el.id);
+const totalGraphSessionPicked = totalVotes.map(el => el.timesPicked);
+const totalGraphSessionSeen = totalVotes.map(el => el.timesSeen);
+
+var totPick = document.getElementById('total-results-picked').getContext('2d');
+new Chart(totPick, {
+    type: 'bar',
+    data: {
+        labels: totalGraphIds,
+        datasets: [{
+            label: '# of Votes',
+            data: totalGraphSessionPicked,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+var totDisplay = document.getElementById('total-results-displayed').getContext('2d');
+new Chart(totDisplay, {
+    type: 'bar',
+    data: {
+        labels: totalGraphIds,
+        datasets: [{
+            label: '# of Times Seen',
+            data: totalGraphSessionSeen,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
 startOverButton.addEventListener('click', () => {
     window.location.href = '../';
     localStorage.removeItem('PICKS');
     localStorage.removeItem('DISPLAYED');
+});
+
+resetLocalStorage.addEventListener('click', () => {
+    window.location.href = './';
+    localStorage.clear();
 });
